1ee7814dfa5e733a9b0653cd6f37033e4c6e8ab5
TiAmowkd <15968320362@163.com>
2019-12-29 22:37:22 +0800

    validation

--- src/test/java/com/ballot/rigging/lombok/DataTest.java
+++ src/test/java/com/ballot/rigging/lombok/DataTest.java
@@ -3,8 +3,9 @@ package com.ballot.rigging.lombok;
 import lombok.Data;
 
 /**
- * @Data注解
- * 大而全的注解：包含@Getter，@Setter，@ToString，@EqualsAndHashCode
+ * @Data注解 大而全的注解：包含@Getter，@Setter，@ToString，@EqualsAndHashCode
+ * @Data = @Setter + @Getter + @EqualsAndHashCode + @NoArgsConstructor + @ToString
+ * （@Data会getter所有的变量）
  */
 @Data
 public class DataTest {
@@ -12,5 +13,7 @@ public class DataTest {
     private String field1;
 
     private String field2;
+
+    private final String field3;
 
 }
--- src/test/java/com/ballot/rigging/lombok/ToStringTest.java
+++ src/test/java/com/ballot/rigging/lombok/ToStringTest.java
@@ -9,9 +9,13 @@ import org.junit.Test;
  * 生成toString方法
  */
 @ToString(
+        //是否在生成的toString（）中生成属性名
         includeFieldNames = false,
+//        此处列出的任何字段都不会在生成的{@code toString}实现中打印。
 //        exclude = {"field1"},
+        //如果存在，则显式列出要打印的字段。
 //        of = {"field1"},
+        //false调用属性时调用属性的get方法
         doNotUseGetters = false
 )
 public class ToStringTest {
