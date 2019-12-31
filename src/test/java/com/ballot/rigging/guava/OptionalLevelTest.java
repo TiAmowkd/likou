a4e65b944f640e020fe77dcaa3256253617171e4
王凯迪 <kaidi.wang@taimei.com>
2019-12-31 14:40:52 +0800

    Optional List应用

--- src/test/java/com/ballot/rigging/guava/OptionalLevelTest.java
+++ src/test/java/com/ballot/rigging/guava/OptionalLevelTest.java
@@ -3,7 +3,11 @@ package com.ballot.rigging.guava;
 import com.ballot.rigging.pojo.Book;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -21,9 +25,7 @@ public class OptionalLevelTest {
         Book book = new Book("john@gmail.com", 1234);
         Optional<Book> opt = Optional.ofNullable(book);
         assertTrue(opt.isPresent());
-
         assertEquals(book.getName(), opt.get().getName());
-
         opt.ifPresent(u -> assertEquals(book.getName(), u.getName()));
     }
 
@@ -42,7 +44,6 @@ public class OptionalLevelTest {
         Book Book = new Book("john@gmail.com", 1234);
         Book Book2 = new Book("anna@gmail.com", 1234);
         Book result = Optional.ofNullable(Book).orElse(Book2);
-
         assertEquals("john@gmail.com", result.getName());
     }
 
@@ -60,8 +61,6 @@ public class OptionalLevelTest {
         System.out.println(optionalBook.get());
         System.out.println(optionalBook.get().getName());
         optionalBook.ifPresent(u -> System.out.println(u.getName()));
-
-
 
        /* newBook为null 不执行u -> System.out.println(u.getName())
         Book newBook = null;
@@ -88,6 +87,60 @@ public class OptionalLevelTest {
                 .map(u -> u.getName()).orElse("default@gmail.com");
         Optional<String> stringOptional = Optional.ofNullable(user2).map(u -> u.getName());
 
+
     }
+
+    @Test
+    public void whenMap_Optional() {
+//        java.util.NoSuchElementException: No value present
+//        Book user2 = null;
+
+//        true
+        Book user2 = new Book("anna@gmail.com", 1234);
+//        java.util.NoSuchElementException: No value present
+//        Book user2 = new Book();
+        Optional<String> stringOptional = Optional.ofNullable(user2).map(u -> u.getName());
+        String s = stringOptional.get();
+        System.out.println(s);
+
+    }
+
+    @Test
+    public void whenMap_BookList() {
+        List<Book> list = new ArrayList<>();
+        list.add(new Book("无人机",4999));
+        list.add(new Book("eeeee",999));
+        list.add(new Book("aede机",495499));
+        list.add(new Book("eefr机",4899));
+        Optional<List<String>> listStringOptional = Optional.ofNullable(list).
+                map(u->u.stream().map(x->x.getName()).collect(Collectors.toList()));
+        List<String> strings = listStringOptional.get();
+        System.out.println(strings);
+
+        //jdk9 Optional.stream()
+        // 注意idea编译环境也要切换为9
+        // setting ->Build、Execution、Deployment -> Compiler -> Java Compiler
+        listStringOptional.stream().forEach(x->x.stream().forEach(u-> System.out.println(u)));
+
+    }
+
+    @Test
+    public void Optional_ListString() {
+        List<String> list = new ArrayList<>();
+        list.add("xxx1");
+        list.add("xxx2");
+        list.add("xxx3");
+
+        Optional<List<String>> optional = Optional.ofNullable(list);
+        optional.stream()//optional 可以转为stream
+                .forEachOrdered(x -> x.stream().forEach(y -> System.out.println(y)));
+
+        list.add("yyy1");
+        Optional<List<String>> optional2 = Optional.ofNullable(list);
+        optional2.stream()
+                .flatMap(x -> x.stream())//optional 扁平化
+                .forEach(System.out::println);
+    }
+
 
 }
